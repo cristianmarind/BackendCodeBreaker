93f3d3d8dc71834a905d9ea65cf08fa98a8b907e
cristian.marind <cristian.marind@udea.edu.co>
2018-06-01 17:24:26 -0500

    First commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 let app = require('./app.js');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 app.listen(PORT,function(){
     console.log("Servidor corriendo bien");
     
--- routes.js
+++ routes.js
@@ -1,20 +1,40 @@
 'use strict' // convencio de EMC6
 const express = require('express');
-const codebreaker= require('./codebreaker');
+const codebreaker = require('./codebreaker');
+const validator = require('./validator');
 //controlador
 const api=express.Router();
-const instanceCodeBreaker = new codebreaker("1234");
+const instanceCodeBreaker = undefined;
 
-api.get('/codebreaker/:num',function (req, res) {
-    let num = req.params.num;
+/*api.get('/codebreaker/:num',function (req, res) {
+    let num = req.body.num;
     let resultado = instanceCodeBreaker.compare(num);
     res.status(200).send({ resultado });
-});
+});*/
 
-api.get('/codebreaker/secret/:num',function (req, res) {
-    let num = req.params.num;
-    let resultado = instanceCodeBreaker.setSecret(num);
-    res.status(200).send({ resultado });
+api.get('/codebreaker/secret/:number',function (req, res) {
+    let response = {
+        "message": "",
+        "err": 1
+    };
+    let validation = validator.validarCadena(req.body.number);
+    if(req.body.number === undefined){
+        console.log("1")
+        response.message = "El require debe de tener el siguiente formato {number: [number]}";
+        return res.status(204).send(response);
+    }
+    if(validation != true){
+        console.log("2")
+        response.message = validation;
+        return res.status(204).send(response);
+    }
+    if(instanceCodeBreaker === undefined){
+        instanceCodeBreaker = new CodeBreaker(req.body.number);
+    }else{
+        instanceCodeBreaker.setSecret(req.body.number);
+    }
+    console.log("3")
+    return res.status(200).send({ resultado });
 });
 
 
