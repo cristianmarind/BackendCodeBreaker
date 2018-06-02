11e501e0e75d7540fd35850084537fbae99183d8
cristian.marind <cristian.marind@udea.edu.co>
2018-06-02 14:19:24 -0500

    Back full

--- routes.js
+++ routes.js
@@ -1,33 +1,47 @@
 'use strict' // convencio de EMC6
 const express = require('express');
-const codebreaker = require('./codebreaker');
+var CodeBreaker = require('./codebreaker');
 const validator = require('./validator');
 //controlador
 const api=express.Router();
-const instanceCodeBreaker = undefined;
+var instanceCodeBreaker = undefined;
 
-/*api.get('/codebreaker/:num',function (req, res) {
-    let num = req.body.num;
-    let resultado = instanceCodeBreaker.compare(num);
-    res.status(200).send({ resultado });
-});*/
-
-api.get('/codebreaker/secret',function (req, res) {
-    console.log(res.params);
-    res.send("asd");
+api.post('/codebreaker/try',function (req, res) {
     let response = {
         "message": "",
         "err": 1
-    }
-    console.log(req);
+    };
     let validation = validator.validarCadena(req.body.number);
     if(req.body.number === undefined){
         response.message = "El require debe de tener el siguiente formato {number: [number]}";
-        return res.status(204).send(response);
+        return res.send({ response });
     }
     if(validation != true){
         response.message = validation;
-        return res.status(204).send(response);
+        return res.send({ response });
+    }
+    if(instanceCodeBreaker === undefined){
+        response.message = "debe de poner un numero secreto"
+        return res.send({ response });
+    }
+    response.err = 0;
+    response.message = instanceCodeBreaker.compare(req.body.number);
+    res.send({ response });
+});
+
+api.post('/codebreaker/secret',function (req, res) {
+    let response = {
+        "message": "",
+        "err": 1
+    };
+    let validation = validator.validarCadena(req.body.number);
+    if(req.body.number === undefined){
+        response.message = "El require debe de tener el siguiente formato {number: [number]}";
+        return res.send({ response });
+    }
+    if(validation != true){
+        response.message = validation;
+        return res.send({ response });
     }
     if(instanceCodeBreaker === undefined){
         instanceCodeBreaker = new CodeBreaker(req.body.number);
@@ -35,8 +49,8 @@ api.get('/codebreaker/secret',function (req, res) {
         instanceCodeBreaker.setSecret(req.body.number);
     }
     response.err = 0;
-    response.message = "Se creó con exito";
-    return res.status(200).send({ response });
+    response.message = "Se creó con exito la calve";
+    return res.send({ response });
 });
 
 
